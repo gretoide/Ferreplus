eec3629219aa5ccfa3c29bd8da8656d0c64a2e68
gretoide <gretaxpiacentini@gmail.com>
2024-04-19 17:23:48 -0300

    Prueba

--- FerrePlus_Project/views.py
+++ FerrePlus_Project/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+import os
 
 # Declaramos una de las vistas de nuestra web, luego hay que agregarla a las urls.
 def inicio(request):
+
     return HttpResponse("Esta sería la página de inicio !!!")
