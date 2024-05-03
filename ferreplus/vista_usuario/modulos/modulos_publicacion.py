eb64e3a872f83d23237e2b3dfb95e008db20c9e9
gretoide <gretaxpiacentini@gmail.com>
2024-05-02 22:57:49 -0300

    Crear publicación funcional

--- ferreplus/vista_usuario/modulos/modulos_publicacion.py
+++ ferreplus/vista_usuario/modulos/modulos_publicacion.py
@@ -1,66 +1,34 @@
 from vista_usuario.models import Publicacion, Imagen
 
-def crear_publicacion(datos_publicacion, imagenes):
+def verificar_campos(datos_publicacion):
     """
-    Crea una nueva publicación a partir de los datos proporcionados.
-    Retorna un booleano que indica si la creación fue exitosa y un mensaje de aviso o error.
+    Verifica si todos los campos necesarios están presentes en los datos de la publicación.
+    Retorna un booleano que indica si la verificación fue exitosa y un mensaje de error en caso contrario.
     """
-    try:
-        nueva_publicacion = Publicacion.objects.create(
-            titulo=datos_publicacion.get('titulo'),
-            estado=datos_publicacion.get('estado'),
-            categoria=datos_publicacion.get('categoria'),
-            sucursal=datos_publicacion.get('sucursal'),
-            descripcion=datos_publicacion.get('descripcion')
-        )
-
-        # Guardar las imágenes asociadas a la publicación
-        for imagen_file in imagenes:
-            imagen = Imagen(publicacion=nueva_publicacion, imagen=imagen_file)
-            imagen.save()
-
-        return True, "Publicación creada con éxito."
-    except Exception as e:
-        return False, str(e)
-
-def verificar_y_crear_publicacion(datos_publicacion, imagenes):
-    """
-    Verifica y crea una nueva publicación a partir de los datos proporcionados.
-    Retorna un booleano que indica si la creación fue exitosa y un mensaje de aviso o error.
-    """
-    # Verificar la publicación
-    condicion, motivo = verificar_publicacion(datos_publicacion)
+    campos_requeridos = ["titulo", "estado", "categoria", "sucursal", "descripcion"]
     
-    if condicion:
-        # Crear la publicación
-        exito, mensaje = crear_publicacion(datos_publicacion, imagenes)
-        return exito, mensaje
-    else:
-        return False, motivo
+    for campo in campos_requeridos:
+        valor_campo = datos_publicacion.get(campo)
+        if not valor_campo or valor_campo.strip().lower() == "seleccionar":
+            return False, "Debe completar todos los campos."
+    return True, None
 
-def verificar_publicacion(publicacion):
-    """
-    Verifica que se hayan proporcionado todos los campos requeridos para una publicación.
-    Retorna un booleano que indica si la verificación fue exitosa y una lista de mensajes de error.
-    """
-    mensajes_error = []
 
-    if not publicacion.get("titulo"):
-        mensajes_error.append("El campo 'Título' es obligatorio.")
+def crear_publicacion(datos_publicacion, imagenes):
 
-    if not publicacion.get("estado"):
-        mensajes_error.append("El campo 'Estado del producto' es obligatorio.")
+    # Crear la nueva publicación
+    nueva_publicacion = Publicacion.objects.create(
+        titulo=datos_publicacion.get('titulo'),
+        estado=datos_publicacion.get('estado'),
+        categoria=datos_publicacion.get('categoria'),
+        sucursal=datos_publicacion.get('sucursal'),
+        descripcion=datos_publicacion.get('descripcion')
+    )
 
-    if not publicacion.get("categoria"):
-        mensajes_error.append("El campo 'Categoría' es obligatorio.")
+    nueva_publicacion.save()
 
-    if not publicacion.get("sucursal"):
-        mensajes_error.append("El campo 'Sucursal' es obligatorio.")
+    # Guardar las imágenes asociadas a la publicación
+    for imagen_file in imagenes:
+        imagen = Imagen(publicacion=nueva_publicacion, imagen=imagen_file)
+        imagen.save()
 
-    if not publicacion.get("descripcion"):
-        mensajes_error.append("El campo 'Descripción' es obligatorio.")
-
-    if mensajes_error:
-        return False, mensajes_error
-    else:
-        return True, ""
--- ferreplus/vista_usuario/urls.py
+++ ferreplus/vista_usuario/urls.py
@@ -3,8 +3,9 @@ from . import views as user_views
 
 urlpatterns = [
     path('registro/', user_views.registro, name= "registro"),
-    path('crear-oferta/', user_views.crear_oferta, name="crear-oferta"),
-    path('pagina-principal/', user_views.subir_publicacion, name="pagina-principal"),
+    path('crear-oferta/', user_views.crear_oferta, name="crear_oferta"),
+    path('pagina-principal/', user_views.pagina_principal, name="pagina_principal"),
+    path('subir-publicacion/', user_views.subir_publicacion, name="subir_publicacion"),
     path('restablecer-contraseña/', user_views.restablecerContraseña, name="restablecer_contraseña"),
     path('ingresar-codigo/<str:email>', user_views.ingresarCodigo, name="ingresar_codigo"),
     path('cambiar-contraseña/<str:email>/<str:contraseña>', user_views.cambiarContraseña, name="cambiar_contraseña"),
