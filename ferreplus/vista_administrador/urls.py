a5f9da4e258a02df74f4200b427c82fb110f8d21
MauricioFRodriguezRicon <mauri030901@gmail.com>
2024-05-13 21:59:34 -0300

    arreglo urls

--- ferreplus/vista_administrador/urls.py
+++ ferreplus/vista_administrador/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from vista_administrador import views as  admin_view
 
 urlpatterns = [
-    #path('registro/', user_views.registro, name= "registro"),
-    path('administrador//', admin_view.inicio_admin, name= "inicio-admin"),
+    path('administrador/', admin_view.inicio_admin, name= "inicio-admin"),
     path('agregar-sucursal/',admin_view.agregar_sucursal, name='agregar-sucursal'),
     path('ver-sucursales/',admin_view.ver_sucursales, name='ver-sucursales'),
     path('detalle-sucursal/<str:sucursal_id>/',admin_view.detalle_sucursal,name='detalle-sucursal')
