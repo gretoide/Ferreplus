0de814fe2dd36332a96c0297d16e4b501939ad41
gretoide <gretaxpiacentini@gmail.com>
2024-05-28 21:12:56 -0300

    Detalles publicación

--- ferreplus/vista_usuario/views.py
+++ ferreplus/vista_usuario/views.py
@@ -110,6 +110,18 @@ def mis_publicaciones(request):
     else:
         return render(request, os.path.join(TEMPLATE_DIR, 'vista_usuario','mis_publicaciones.html'), {'publicaciones': publicaciones})
 
+def detalle_publicacion(request, publicacion_id):
+    publicacion = get_object_or_404(Publicacion, id=publicacion_id)
+    imagenes = publicacion.imagenes.all()
+
+    contexto = {
+        'publicacion': publicacion,
+        'imagenes': imagenes,
+    }
+
+    return render(request, os.path.join(TEMPLATE_DIR, 'vista_usuario','detalle_publicacion.html'), contexto)
+
+
 @login_required
 @normal_required
 def editar_publicacion(request, pk):
