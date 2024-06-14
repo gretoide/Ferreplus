c608b23c075784a3b8fe92df4e60129c6a5a9639
gretoide <gretaxpiacentini@gmail.com>
2024-06-14 18:04:14 -0300

    Verificar que sean de la misma categoria

--- ferreplus/vista_usuario/modulos/modulos_oferta.py
+++ ferreplus/vista_usuario/modulos/modulos_oferta.py
@@ -16,7 +16,17 @@ def validar_hora_encuentro(hora_encuentro):
         return False, 'El horario del encuentro debe ser entre las 8 a.m. y las 6 p.m.'
     return True, ''
 
+def validar_categoria(categoria_base, categoria_ofertada):
+    if categoria_base == categoria_ofertada:
+        return True, ''
+    else:
+        return False, 'Los productos deben ser de la misma categoría.'
+
 def procesar_oferta(publicacion_base, usuario, publicacion_id, fecha_encuentro, hora_encuentro):
+
+    # Obtén la publicación seleccionada
+    publicacion_seleccionada = get_object_or_404(Publicacion, id=publicacion_id)
+
     # Validar fecha del encuentro
     valido_fecha, mensaje_fecha = validar_fecha_encuentro(fecha_encuentro)
     if not valido_fecha:
@@ -26,12 +36,14 @@ def procesar_oferta(publicacion_base, usuario, publicacion_id, fecha_encuentro, 
     valido_hora, mensaje_hora = validar_hora_encuentro(hora_encuentro)
     if not valido_hora:
         return mensaje_hora, False
+    
+    # Validar que sean de la misma categoría
+    valido_categoria, mensaje_categoria = validar_categoria(publicacion_base.categoria, publicacion_seleccionada.categoria)
+    if not valido_categoria:
+        return mensaje_categoria, False
 
+    # Crear la oferta si todas las validaciones son exitosas
     if publicacion_id and fecha_encuentro and hora_encuentro:
-        # Obtén la publicación seleccionada
-        publicacion_seleccionada = get_object_or_404(Publicacion, id=publicacion_id) # type: ignore
-
-        # Crea una nueva oferta
         Oferta.objects.create(
             base=publicacion_base,
             oferta=publicacion_seleccionada,
@@ -41,8 +53,8 @@ def procesar_oferta(publicacion_base, usuario, publicacion_id, fecha_encuentro, 
             hora=hora_encuentro
         )
         return '', True
-
-    return 'Debe seleccionar una publicación para el intercambio.', False
+    else:
+        return 'Debe seleccionar una publicación para el intercambio, fecha y hora válidas.', False
 
 def procesar_oferta_privada(publicacion_base, publicacion_nueva, fecha_encuentro, hora_encuentro):
     # Validar fecha del encuentro
